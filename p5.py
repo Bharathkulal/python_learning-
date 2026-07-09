990f536a7f25e064734063ad41a3b6496e52e158
Bharath Kulal <bharathkulal2007@gmail.com>
2026-07-09 10:50:24 +0530

    day 5

---
+++ p5.py
@@ -0,0 +1,27 @@
+#lists
+
+items= ["apple", "bannana", "cherry", "date"]
+
+print(items[2])
+
+print(items.pop())
+print(items)
+print(items.append("kivi"))
+print(items.remove("apple"))
+
+
+print(items)
+
+
+
+
+
+
+
+#slacing
+
+lists=[22,66,44,33,11,77,99,88,55]
+
+print(len(lists))
+
+print(sorted(lists))
