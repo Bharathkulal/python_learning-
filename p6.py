55547a09bda6b6523a691e9aabb504eb3e34f7e3
Bharath Kulal <bharathkulal2007@gmail.com>
2026-07-10 09:41:30 +0530

    day 6

---
+++ p6.py
@@ -0,0 +1,25 @@
+#tupes and sets 
+
+#TUPLES
+
+genders = ("male", "female", "others")
+print(genders )
+
+#creating a tuples in one elements
+
+genders1= ("apples",)
+print(genders1)
+
+print(type(genders))
+print(len(genders))
+
+
+#sets
+
+sets = {102,303,567,389,456}
+
+print(sets)
+print(type(sets))
+print(len(sets))
+print(sorted(sets))
+print(sets.pop())
